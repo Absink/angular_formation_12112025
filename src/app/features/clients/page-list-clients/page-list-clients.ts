f4c4ed238c9b4cabd119befd34ad9465735dfd27
Absink <absink@absink.fr>
2025-11-13 16:40:38 +0100

    Implemente computed()

--- src/app/features/clients/page-list-clients/page-list-clients.ts
+++ src/app/features/clients/page-list-clients/page-list-clients.ts
@@ -1,7 +1,7 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { ClientService } from '../../../shared/services/client-service';
 import { Client } from '../../../shared/models/client.model';
-import { CurrencyPipe, JsonPipe } from '@angular/common';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-page-list-clients',
@@ -13,14 +13,19 @@ export class PageListClients {
 
   private clientService = inject(ClientService);
 
-  public clients: Client[] = [];
-  public totalCA = signal(0);
+  public clients = signal<Client[]>([]);
+  public totalCA = computed(() => {
+    return this.clients().reduce((acc, client) => acc + client.ca, 0)
+  });
+  public totalCAWithBonus = computed(() => {
+    return this.clients().reduce((acc, client) => acc + (client.ca * 1.1 + 1500), 0)
+  })
+
 
   ngOnInit(): void {
     this.clientService.getAll().subscribe({
       next: (datas) => {
-        this.clients = datas;
-        this.totalCA.set(this.clients.reduce((acc, client) => acc + client.ca, 0));
+        this.clients.set(datas);
       },
       error: (err) => console.log(err)
     });
@@ -29,7 +34,9 @@ export class PageListClients {
   remove(id: number): void {
     this.clientService.deleteById(id).subscribe({
       next: () => {
-        // this.clients.find(c => c.id == id);
+        this.clients.update((clients) => 
+          clients.filter((client) => client.id !== id)
+        )
       }
     });
   }
